272fbaa56fcf9b7b2f613a84b7bc3570d9ca3aa5
leemoonstudio <djarma.kin@gmail.com>
2023-12-26 20:16:04 +0100

    update

--- config/plugins.js
+++ config/plugins.js
@@ -8,4 +8,20 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  // upload: {
+  //   enabled: true,
+  //   config: {
+  //     provider: "cloudinary",
+  //     providerOptions: {
+  //       cloud_name: env("CLOUDINARY_NAME"),
+  //       api_key: env("CLOUDINARY_KEY"),
+  //       api_secret: env("CLOUDINARY_SECRET"),
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
 });
