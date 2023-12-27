4bc1556873de912c8be87f6231cf87b8fc1edede
leemoonstudio <djarma.kin@gmail.com>
2023-12-27 13:57:57 +0100

    middleware security

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,6 @@
 module.exports = [
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "script-src": process.env.STRAPI_SECURITY_SCRIPT_SRC.split(", "),
-          "img-src": process.env.STRAPI_SECURITY_IMG_SRC.split(", "),
-          "media-src": process.env.STRAPI_SECURITY_MEDIA_SRC.split(", "),
-          "connect-src": process.env.STRAPI_SECURITY_CONNECT_SRC.split(", "),
-        },
-        upgradeInsecureRequests: null,
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
