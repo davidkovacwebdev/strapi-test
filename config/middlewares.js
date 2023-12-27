0a23b4002c771602c0aaa8072ee4c9899652222e
leemoonstudio <djarma.kin@gmail.com>
2023-12-27 13:56:31 +0100

    middleware update

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,17 +6,10 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "script-src": ["self", "unsafe-inline", "cdn.jsdelivr.net", "blob:"],
-          "img-src": [
-            "self",
-            "data:",
-            "strapi.io",
-            "cdn.jsdelivr.net",
-            "res.cloudinary.com",
-            "market-assets.strapi.io",
-          ],
-          "media-src": ["self", "data", "blob:", "res.cloudinary.com"],
-          "connect-src": ["self", "http:", "https:"],
+          "script-src": process.env.STRAPI_SECURITY_SCRIPT_SRC.split(", "),
+          "img-src": process.env.STRAPI_SECURITY_IMG_SRC.split(", "),
+          "media-src": process.env.STRAPI_SECURITY_MEDIA_SRC.split(", "),
+          "connect-src": process.env.STRAPI_SECURITY_CONNECT_SRC.split(", "),
         },
         upgradeInsecureRequests: null,
       },
